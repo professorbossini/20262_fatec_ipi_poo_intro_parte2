ec220d5fdccfe1fb2278535f15c6a58f0af79343
Rodrigo Bossini <professorbossini@gmail.com>
2026-09-10 17:27:12 -0300

    learn(ap006-parte2): propoe exercicios

--- src/main/java/br/com/bossini/JogoV4.java
+++ src/main/java/br/com/bossini/JogoV4.java
@@ -6,4 +6,8 @@ public class JogoV4 {
   //ele caça com probabilidade 50%
   //dorme com probabilidade 30%
   //come com probabilidade 20%
+
+  //fazer o personagem morrer quando a energia chegar a zero. o jogo acaba ai
+  //contar o numero de rodadas e exibir no final
+  //implemente um placar: quando o personagem caçar, comer ou dormir em uma rodada, ele ganha uma vitória é contabilizada. quando tentar fazer uma atividade e não tiver energia, fome ou sono para fazê-lo, uma derrota é contabilizada. exiba os números de vitória e derrota a cada rodada
 }
